adb408d19e28163ebdd9eecaa0d2d409ae116015
Scott Gallant <scottgallant@gmail.com>
2024-10-07 14:25:34 -0300

    fixed bugs

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,22 @@
+import React from "react";
+
 function Form() {
+  const [guess, setGuess] = React.useState(" ");
   return (
-    <form class="guess-input-wrapper">
-      <label for="guess-input">Enter guess:</label>
-      <input id="guess-input" type="text" />
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(guess);
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(event) => setGuess(event.target.value)}
+      />
     </form>
   );
 }
