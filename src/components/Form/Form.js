b432a2492b56c13ed074f75d791134fec701e3f5
Scott Gallant <scottgallant@gmail.com>
2024-10-07 12:55:59 -0300

    new form component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,10 @@
+function Form() {
+  return (
+    <form class="guess-input-wrapper">
+      <label for="guess-input">Enter guess:</label>
+      <input id="guess-input" type="text" />
+    </form>
+  );
+}
+
+export default Form;
