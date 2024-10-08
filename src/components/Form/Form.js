192b2adfa1cd135f00ac069c86964113b38950d3
Scott Gallant <scottgallant@gmail.com>
2024-10-07 23:08:46 -0300

    finished excercise 1

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,21 +1,26 @@
 import React from "react";
 
 function Form() {
-  const [guess, setGuess] = React.useState(" ");
+  const [guess, setGuess] = React.useState("");
   return (
     <form
       className="guess-input-wrapper"
       onSubmit={(event) => {
         event.preventDefault();
         console.log(guess);
+        setGuess("");
       }}
     >
       <label htmlFor="guess-input">Enter guess:</label>
       <input
+        required // This attribute makes the input field mandatory.
         id="guess-input"
         type="text"
         value={guess}
-        onChange={(event) => setGuess(event.target.value)}
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+        maxLength="5"
+        title="Please enter a 5-letter word."
+        pattern="[A-Za-z]{5}"
       />
     </form>
   );
